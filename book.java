3667cc9b44fc983dac0b42199f4103828af6a1f1
ISsam777 <issam392004@gmail.com>
2025-05-04 13:33:46 +0100

    edit

---
+++ book.java
@@ -0,0 +1,23 @@
+public class book {
+	private String name;
+	private String writer;
+	private String editor;
+	private int quantity;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEditor() {
+		return editor;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+}
